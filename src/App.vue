ed73f804fc1fc1419e669a69c1512fa360c939ce
tianmin <tianmin@pku.edu.cn>
2020-02-05 23:49:17 +0800

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <template>
   <div id="app">
     <div id="min_div">
-      <tian-min/>
+      <tian-min />
     </div>
   </div>
 </template>
 
 <script>
-import TianMin from "./components/TianMin"
+import TianMin from "./components/TianMin";
 export default {
-  name: 'app',
+  name: "app",
   components: {
     TianMin
   }
-}
+};
 </script>
 
-<style>
+<style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  // color: #2c3e50;
   margin-top: 60px;
 }
 
 #min_div {
   position: absolute;
   width: 800px;
-  border: 1px solid red;
+  // border: 1px solid red;
 }
 </style>
